ccdf8d74ac0c675a505a6c2763001bd393e91911
unknown <alajfit@gmail.com>
2015-05-22 12:30:26 +0100

    Additional Changes to the Template

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-  	title: 'Support | API Requester',
+  	title_name: 'Support | API Requester',
+  	title_logo_Image: '/public/images/logo/api.png',
   	author: 'Alan J. Fitzpatrick'
   });
 });
